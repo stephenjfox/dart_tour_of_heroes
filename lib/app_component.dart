e1d058b7aff55e5fa7133901cd205d2031d92fc4
Stephen Fox <stevethecoder34@gmail.com>
2017-01-11 18:52:26 -0700

    Complete Tutorial Part 1
    The tutorial in question is Tour of Heroes -
    https://webdev.dartlang.org/angular/tutorial/toh-pt1

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -7,7 +7,10 @@ import 'package:angular2/core.dart';
     <h2>{{hero.name}} details!</h2>
 
     <div><label>id: </label> {{hero.id}}</div>
-    <div><label>name: </label> {{hero.name}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
     '''
 )
 class AppComponent {
