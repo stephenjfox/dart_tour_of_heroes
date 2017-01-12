e95e73f6c1bcdfd5e486ea0229a92a9889a9436d
Stephen Fox <stevethecoder34@gmail.com>
2017-01-11 18:45:33 -0700

    Initial (roughly) commit

---
+++ lib/app_component.dart
@@ -0,0 +1,24 @@
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'my-app',
+    template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+
+    <div><label>id: </label> {{hero.id}}</div>
+    <div><label>name: </label> {{hero.name}}</div>
+    '''
+)
+class AppComponent {
+  String title = "Tour of Heroes";
+  var hero = new Hero(1, "Windstrom");
+}
+
+
+class Hero {
+  final int id;
+  String name;
+
+  Hero(this.id, this.name);
+}
---
+++ web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/platform/browser.dart';
+
+import 'package:dart_tour_of_heroes/app_component.dart';
+
+void main() {
+  bootstrap(AppComponent);
+}
