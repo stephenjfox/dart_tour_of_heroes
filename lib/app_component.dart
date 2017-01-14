06c3a07e27e9d1544a693489717b4604f084a67c
Stephen Fox <stevethecoder34@gmail.com>
2017-01-13 19:35:06 -0700

    Add dashboard default routing
    Need to allow deep linking and expansion of the 'Top Heroes'
      display. Using the HeroDetailsComponent for that extra

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,17 +2,27 @@ import 'package:angular2/core.dart';
 import 'package:angular2/router.dart';
 import 'hero_service.dart';
 import 'heroes_component.dart';
+import 'dashboard_component.dart';
 
 @Component(
     selector: 'my-app',
     template: '''
       <h1>{{title}}</h1>
-      <a [routerLink]="['Heroes']">Heroes</a>
+      <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+      </nav>
       <router-outlet></router-outlet>''',
     directives: const [ROUTER_DIRECTIVES],
     providers: const [HeroService, ROUTER_PROVIDERS])
 @RouteConfig(const [
-  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent)
+  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent),
+  const Route(
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  )
 ])
 class AppComponent {
   final String title = "Tour of Heroes";
