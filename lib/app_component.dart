85177faffd7db2132c6936364c2f78084d0ead45
Stephen Fox <stevethecoder34@gmail.com>
2017-01-13 19:21:59 -0700

    Add the RouteConfig to AppComponent

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,4 +1,5 @@
 import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
 import 'hero_service.dart';
 import 'heroes_component.dart';
 
@@ -6,9 +7,13 @@ import 'heroes_component.dart';
     selector: 'my-app',
     template: '''
       <h1>{{title}}</h1>
-      <my-heroes></my-heroes>''',
-    directives: const [HeroesComponent],
-    providers: const [HeroService])
+      <a [routerLink]="['Heroes']">Heroes</a>
+      <router-outlet></router-outlet>''',
+    directives: const [ROUTER_DIRECTIVES],
+    providers: const [HeroService, ROUTER_PROVIDERS])
+@RouteConfig(const [
+  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent)
+])
 class AppComponent {
   final String title = "Tour of Heroes";
 }
