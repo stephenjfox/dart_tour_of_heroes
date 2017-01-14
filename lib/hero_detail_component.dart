66c9fd82de7de28f241b74b8aec5d15743c84425
Stephen Fox <stevethecoder34@gmail.com>
2017-01-14 08:03:38 -0700

    HeroDetail is navigated to and Service.getHero(id: int)

--- lib/hero_detail_component.dart
+++ lib/hero_detail_component.dart
@@ -1,21 +1,34 @@
+import 'dart:async';
+
 import 'package:angular2/core.dart';
+import 'package:angular2/platform/common.dart';
+import 'package:angular2/router.dart';
 
 import 'hero.dart';
+import 'hero_service.dart';
 
 @Component(
   selector: 'my-hero-detail',
-  template: '''
-    <div *ngIf="hero != null">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label> {{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-  '''
+  templateUrl: 'hero_detail_component.html'
 )
-class HeroDetailComponent {
+class HeroDetailComponent implements OnInit {
+
   @Input()
   Hero hero;
+
+  final HeroService _heroService;
+  final RouteParams _routeParams;
+  final Location _location;
+
+
+  HeroDetailComponent(this._heroService, this._routeParams, this._location);
+
+  void goBack() => _location.back();
+
+  @override
+  Future<Null> ngOnInit() async {
+    var idParam = _routeParams.get('id');
+    var id = int.parse(idParam ?? '', onError: (_) => null);
+    if (id != null) hero = await (_heroService.getHero(id));
+  }
 }
