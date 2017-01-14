895bd57b2998be101d8a0e483f767f45195a109c
Stephen Fox <stevethecoder34@gmail.com>
2017-01-13 18:23:05 -0700

    app_component.dart > heroes_component.dart

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular2/platform/browser.dart';
 
-import 'package:dart_tour_of_heroes/app_component.dart';
+import 'package:dart_tour_of_heroes/heroes_component.dart';
 
 void main() {
   bootstrap(HeroesComponent);
