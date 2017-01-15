f20fd2db773dd3ac17fa7084479b144390e52e08
Stephen Fox <stevethecoder34@gmail.com>
2017-01-15 09:53:50 -0700

    Remove mock_heroes and send mock requests instead
    The naming is a little tough here:
      - HeroService serves Heroes to components
      - InMemoryDataService serves data to the HeroService
        ^^ See what I mean?
      It might be better to call it "InMemoryHttpEndpoint", but I'm sure
        that is non-conventional.

--- lib/hero_service.dart
+++ lib/hero_service.dart
@@ -1,13 +1,40 @@
 import 'dart:async';
+import 'dart:convert';
+
 import 'package:angular2/core.dart';
+import 'package:http/http.dart';
 
 import 'hero.dart';
-import 'mock_heroes.dart';
 
 @Injectable()
 class HeroService {
-  Future<List<Hero>> getHeroes() async => mockHeroes;
+  static const _heroesUrl = "api/heroes";
 
+  final Client _http;
+
+  HeroService(this._http);
+
+  /// Using our client service, fetch all the heroes we can get
+  Future<List<Hero>> getHeroes() async {
+    try {
+      final response = await _http.get(_heroesUrl);
+      final heroes = _extractData(response)
+          .map((value) => new Hero.fromJson(value))
+          .toList();
+      return heroes;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  /// Fetch a hero from our API, individually by the id
   Future<Hero> getHero(int id) async =>
       (await getHeroes()).firstWhere((hero) => hero.id == id);
+
+  dynamic _extractData(Response resp) => JSON.decode(resp.body)['data'];
+
+  Exception _handleError(dynamic e) {
+    print(e); // for demo purposes only
+    return new Exception('Server error; cause: $e');
+  }
 }
